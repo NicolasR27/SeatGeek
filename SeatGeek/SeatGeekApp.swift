c6db2f937c63720a7e5d7fbab5009aec7ce402ae
Nicolas Rios <Nickrios33@yahoo.com>
2022-05-04 15:18:41 -0700

    Initial Commit

---
+++ SeatGeek/SeatGeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeatGeekApp.swift
+//  SeatGeek
+//
+//  Created by Nicolas Rios on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct SeatGeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
